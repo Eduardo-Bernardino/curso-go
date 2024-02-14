f3a5ddc9ce2f0d82661455c7cedd51cd75836e67
Eduardo-Bernardino <eduardomsbdev@gmail.com>
2024-02-14 14:23:03 -0300

    terceira aula parte 1, com pacote importado

--- 1- Pacotes/main.go
+++ 1- Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste@ufcat.com.br")
+	fmt.Println(erro)
 }
