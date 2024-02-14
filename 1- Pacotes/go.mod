cb3c0e4836b34cedcfb2ec1c91ab174a26324666
Eduardo-Bernardino <eduardomsbdev@gmail.com>
2024-02-14 14:24:58 -0300

    terceira aula final

--- 1- Pacotes/go.mod
+++ 1- Pacotes/go.mod
@@ -1,5 +1,3 @@
 module modulo
 
 go 1.22.0
-
-require github.com/badoux/checkmail v1.2.4 // indirect
--- 1- Pacotes/main.go
+++ 1- Pacotes/main.go
@@ -3,14 +3,9 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
-
-	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
-
-	erro := checkmail.ValidateFormat("teste@ufcat.com.br")
-	fmt.Println(erro)
 }
