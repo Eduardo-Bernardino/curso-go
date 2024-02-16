8a8dbb046ba44024b4cab8ff2a82992f01a7b71b
Eduardo-Bernardino <eduardomsbdev@gmail.com>
2024-02-16 14:00:34 -0300

    decima primeira aula, array e slice

---
+++ 9- Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*Arrays possuem uma estrutura muito rigida e fixa, sendo sempre necessario colocar
+	tamanho e tipo na sua tribuição e desta forma não é muito utilizado na linguagem*/
+	fmt.Println("Arrays")
+
+	var array1 [5]int //como não foi atribuido valores no array ele coloca tudo como 0
+	fmt.Println(array1)
+
+	//Populando o array
+	array1[0] = 25
+	fmt.Println(array1)
+
+	//Obs: sempre tem q expecificar o tamanho do array, pois se n expecificar ele vira um slice
+
+	array2 := [5]string{} /*outra forma de declarar um array, declarando tamanho e tipo,
+	porem tem q especificar o que vai nele. No caso de array de strings, se não declarar nada
+	ele atribui apenas preenche com string vazia*/
+	fmt.Println(array2)
+
+	array3 := [5]string{"p1", "p2", "p3", "p4", "p5"} //o array pode ser inicializado na sua declaração
+	fmt.Println(array3)
+
+	array4 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array4)
+	/*outra forma de declarar um array, é com o "..." como tamanho, desta forma ele fixa o tamanho do array
+	de acordo com o tamanho que eu inicializei ele na atribuição*/
+
+	/*Sliced permitem uma flexibilidade no tamanho, porem ainda possui atribuição fixa de tipo*/
+	fmt.Println("Slices")
+
+	slice1 := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice1)
+
+	/*Slices não são como arrays na sua atribuição, o slice aponta para uma parte na memoria
+	onde está os dados em forma de array,*/
+
+	slice1 = append(slice1, 6) /*Como o slice não é fixo, a função append apenas adiciona o novo dado no final do "array",
+	então ele copia todos os dados e retorna um novo "array" com o novo item adicionado na devida posição, ou seja,
+	ele cria um outro "array" em outro lugar copia tudo o que tinha no antigo, adiciona o novo item e te devolve o novo*/
+	fmt.Println(slice1)
+
+	/*slice pode ser meio q traduzido do ingles, como um pedaço, levando meio q o sentido da coisa, da para pegar partes de array
+	e colocar para ele*/
+
+	slice2 := array3[1:4] /* o segundo parametro é onde ele vai pagar, no caso no indice 4. O slide tbm é como se fosse um ponteiro
+	apontando para essas posicoes do array*/
+	fmt.Println(slice2)
+
+	array3[2] = "p11"
+	array3[3] = "p99"
+	fmt.Println(array3)
+	fmt.Println(slice2)
+
+}
