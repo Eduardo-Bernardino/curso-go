8fb744c8b236f6940e01abedf409052b9dcf08cd
Eduardo-Bernardino <eduardomsbdev@gmail.com>
2024-02-21 08:46:58 -0300

    Aula sobre fundamentos HTTP

---
+++ 19- HTTP/http.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func raiz(w http.ResponseWriter, r *http.Request) { // raiz
+	w.Write([]byte("Página Raiz!"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá Mundo!"))
+}
+
+func usuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Carregar página de usuários!"))
+}
+
+func main() {
+	// HTTP É UM PROTOCOLO DE COMUNICAÇÃO - BASE DA COMUNICAÇÃO WEB
+
+	// CLIENTE (FAZ REQUISIÇÃO) - SERVIDOR (PROCESSA REQUISIÇÃO E ENVIA RESPOSTA)
+
+	// Request - Response
+
+	// Rotas
+	// URI - Identificador do Recurso
+	// Método - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/", raiz)
+
+	http.HandleFunc("/home", home)
+
+	http.HandleFunc("/usuarios", usuario)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
