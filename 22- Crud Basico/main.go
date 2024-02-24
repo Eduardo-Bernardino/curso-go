e65b3c7ae813a9fafd26ef53038fa3f049d61a15
Eduardo-Bernardino <eduardomsbdev@gmail.com>
2024-02-24 18:51:21 -0300

    crud basico, metodos get, get e get one

--- 22- Crud Basico/main.go
+++ 22- Crud Basico/main.go
@@ -12,6 +12,8 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/user", servidor.CriateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", servidor.FindUsers).Methods(http.MethodGet)
+	router.HandleFunc("/user/{id}", servidor.FindUser).Methods(http.MethodGet)
 
 	fmt.Println("Rodando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- 22- Crud Basico/servidor/servidor.go
+++ 22- Crud Basico/servidor/servidor.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type user struct {
@@ -14,6 +17,7 @@ type user struct {
 	Email string `json:email`
 }
 
+// Cria e insere o usuario no banco
 func CriateUser(w http.ResponseWriter, r *http.Request) {
 	corpo, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
@@ -59,4 +63,85 @@ func CriateUser(w http.ResponseWriter, r *http.Request) {
 	//Status code
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("User criado com sucesso! ID: %d", idInserido)))
+}
+
+// Busca todos os usuarios do banco
+func FindUsers(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco"))
+		return
+	}
+	defer db.Close()
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar usuarios no banco"))
+		return
+	}
+	defer linhas.Close()
+
+	var users []user
+
+	for linhas.Next() {
+		var user user
+
+		if erro := linhas.Scan(&user.ID, &user.Nome, &user.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuario no banco"))
+			return
+		}
+		users = append(users, user)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(users); erro != nil {
+		w.Write([]byte("Erro converter usuarios para json"))
+		return
+	}
+}
+
+// Busca apenas 1 usuario expecifico no banco
+func FindUser(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 64)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parametro para int"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco"))
+		return
+	}
+	defer db.Close()
+
+	linha, erro := db.Query("select * from usuarios where id = ?", ID)
+	if erro != nil {
+		w.Write([]byte("Erro ao encontrar o usuario no banco"))
+		return
+	}
+	defer linha.Close()
+
+	var user user
+	if linha.Next() {
+		if erro := linha.Scan(&user.ID, &user.Nome, &user.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuario no banco"))
+			return
+		}
+	}
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Usuario com o id informado não foi encontrado no banco"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(user); erro != nil {
+		w.Write([]byte("Erro converter o usuario para json"))
+		return
+	}
+
 }
